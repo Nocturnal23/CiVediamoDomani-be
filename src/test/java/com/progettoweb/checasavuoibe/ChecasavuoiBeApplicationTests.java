4ab729b82fc2f47ba6181f00df7862429e738791
Nocturnal23 <matarazzogennaro1997@gmail.com>
2022-06-16 20:48:48 +0200

    Initial commit

---
+++ src/test/java/com/progettoweb/checasavuoibe/ChecasavuoiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progettoweb.checasavuoibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChecasavuoiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

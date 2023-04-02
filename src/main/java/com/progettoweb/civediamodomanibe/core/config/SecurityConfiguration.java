f357545b76161fe22af681c75f6dabf6e0a32ce2
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-04-02 12:53:54 +0200

    Setting up project authentication

--- src/main/java/com/progettoweb/civediamodomanibe/core/config/SecurityConfiguration.java
+++ src/main/java/com/progettoweb/civediamodomanibe/core/config/SecurityConfiguration.java
@@ -1,66 +1,19 @@
 package com.progettoweb.civediamodomanibe.core.config;
 
-import com.google.common.collect.ImmutableList;
-import com.progettoweb.civediamodomanibe.core.config.authfilters.JwtAuthFilter;
-import com.progettoweb.civediamodomanibe.core.config.authfilters.UsernamePasswordAuthFilter;
-import com.progettoweb.civediamodomanibe.core.utils.Constants;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.access.AccessDeniedException;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfiguration {
-
-    private static final Long MAX_AGE = 3600L;
-    private static final int CORS_FILTER_ORDER = -102;
-
-    private final UserAuthenticationProvider userAuthenticationProvider;
-
-    public SecurityConfiguration(UserAuthenticationProvider userAuthenticationProvider) {
-        this.userAuthenticationProvider = userAuthenticationProvider;
-    }
-
     @Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(ImmutableList.of("*"));
-        config.setAllowedHeaders(ImmutableList.of("*"));
-        config.setAllowedMethods(ImmutableList.of(
-                HttpMethod.GET.name(),
-                HttpMethod.POST.name(),
-                HttpMethod.PUT.name(),
-                HttpMethod.DELETE.name(),
-                HttpMethod.POST.name()));
-        config.setMaxAge(MAX_AGE);
-//        config.setExposedHeaders(
-//                ImmutableList.of(
-//                        "X-Current-Page, X-Num-Current-Page-Elements, X-Num-Total-Pages, X-Num-Total-Elements, X-Request-Outcome",
-//                        "Content-Type",
-//                        "api_key",
-//                        "Authorization"));
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean<>( new CorsFilter(source));
-        bean.setOrder(CORS_FILTER_ORDER);
-        return bean;
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
     }
 
 //    @Override
@@ -87,57 +40,10 @@ public class SecurityConfiguration {
 //                        "/h2-console/**");
 //    }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .exceptionHandling().authenticationEntryPoint(authEntryPoint())
-//                .accessDeniedHandler(accessDeniedHandler())
-//                .and()
-//                .addFilterBefore(new UsernamePasswordAuthFilter(userAuthenticationProvider), CsrfFilter.class)
-//                .addFilterBefore(new JwtAuthFilter(userAuthenticationProvider), UsernamePasswordAuthFilter.class)
-//                .csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-////                .headers().frameOptions().sameOrigin()
-////                .and()
-//                .authorizeRequests()
-//
-//                .antMatchers("**").hasRole(Constants.StringRole.AMMINISTRATORE)
-//                .antMatchers("/login*").permitAll()
-//                .anyRequest().authenticated();
-//    }
-
-    @Slf4j
-    static class AccessDeniedCustomHandler implements AccessDeniedHandler {
-        @Override
-        public void handle(
-                HttpServletRequest request,
-                HttpServletResponse response,
-                AccessDeniedException accessDeniedException)
-                throws IOException {
-            log.error(accessDeniedException.getMessage());
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
-        }
-    }
-
-    static class AuthEntryPoint implements AuthenticationEntryPoint {
-        @Override
-        public void commence(
-                HttpServletRequest request,
-                HttpServletResponse response,
-                AuthenticationException authException) throws IOException {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
-        }
-
-    }
-
     @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        return new AccessDeniedCustomHandler();
-    }
-
-    @Bean
-    public AuthEntryPoint authEntryPoint() {
-        return new AuthEntryPoint();
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests().requestMatchers("/api/v1/register", "/api/v1/authenticate").permitAll().and().
+                authorizeHttpRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable().build();
     }
 }

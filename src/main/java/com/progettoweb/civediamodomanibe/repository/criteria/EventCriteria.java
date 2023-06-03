5827d6eb4f310001087d2505e60143e87528f20a
Nocturnal23 <matarazzogennaro1997@gmail.com>
2023-06-03 18:02:25 +0200

    Added title event filter.

--- src/main/java/com/progettoweb/civediamodomanibe/repository/criteria/EventCriteria.java
+++ src/main/java/com/progettoweb/civediamodomanibe/repository/criteria/EventCriteria.java
@@ -9,4 +9,5 @@ import lombok.Setter;
 @Getter
 @Setter
 public class EventCriteria extends BaseCriteria {
+    private String searchValue;
 }
--- src/main/java/com/progettoweb/civediamodomanibe/repository/specification/EventSpecificationBuilder.java
+++ src/main/java/com/progettoweb/civediamodomanibe/repository/specification/EventSpecificationBuilder.java
@@ -6,11 +6,19 @@ import com.progettoweb.civediamodomanibe.repository.criteria.EventCriteria;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Component
 public class EventSpecificationBuilder extends SpecificationBuilder<Event, EventCriteria> {
     @Override
     public Specification<Event> filter(EventCriteria criteria) {
         Specification<Event> specification = super.filter(criteria);
+
+        if( criteria.getSearchValue() != null ) {
+            specification = Objects.requireNonNull(specification).and(
+                    ((root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), "%"+criteria.getSearchValue()+"%"))
+            );
+        }
 
         return specification;
     }

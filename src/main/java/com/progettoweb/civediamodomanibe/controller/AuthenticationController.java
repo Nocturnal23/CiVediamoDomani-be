ab9f15da15e475b8259c4c0bb7da445d94c2bc82
Alessio Rotondaro <alessio.rotondaro@ietechnology>
2023-06-11 21:09:41 +0200

    Code cleanup: deleted unused authenticate method, unused imports and signout endpoint

--- src/main/java/com/progettoweb/civediamodomanibe/controller/AuthenticationController.java
+++ src/main/java/com/progettoweb/civediamodomanibe/controller/AuthenticationController.java
@@ -3,15 +3,12 @@ package com.progettoweb.civediamodomanibe.controller;
 import com.progettoweb.civediamodomanibe.core.utils.Constants.Endpoint;
 import com.progettoweb.civediamodomanibe.dto.SocialUserDto;
 import com.progettoweb.civediamodomanibe.dto.UserDto;
-import com.progettoweb.civediamodomanibe.dtorequest.CredentialsDto;
 import com.progettoweb.civediamodomanibe.service.AuthenticationService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
